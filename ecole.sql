54581a9c5c9d669b54f5c0871c8d0bb6e7cab8d2
Dobid <davidolivares68@gmail.com>
2019-05-23 14:39:48 +0200

    Create ecole.sql

---
+++ ecole.sql
@@ -0,0 +1,229 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  jeu. 23 mai 2019 à 12:37
+-- Version du serveur :  5.7.24
+-- Version de PHP :  7.2.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `ecole`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `anneescolaire`
+--
+
+DROP TABLE IF EXISTS `anneescolaire`;
+CREATE TABLE IF NOT EXISTS `anneescolaire` (
+  `id_annee` int(11) NOT NULL AUTO_INCREMENT,
+  `id_trimestre` int(11) NOT NULL,
+  PRIMARY KEY (`id_annee`),
+  KEY `id_trimestre` (`id_trimestre`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `bulletin`
+--
+
+DROP TABLE IF EXISTS `bulletin`;
+CREATE TABLE IF NOT EXISTS `bulletin` (
+  `id_bulletin` int(11) NOT NULL AUTO_INCREMENT,
+  `id_trimestre` int(11) NOT NULL,
+  `id_inscription` int(11) NOT NULL,
+  `appreciation` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_bulletin`),
+  KEY `id_trimestre` (`id_trimestre`),
+  KEY `id_inscription` (`id_inscription`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `classe`
+--
+
+DROP TABLE IF EXISTS `classe`;
+CREATE TABLE IF NOT EXISTS `classe` (
+  `id_classe` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) DEFAULT NULL,
+  `id_ecole` int(11) NOT NULL,
+  `id_niveau` int(11) NOT NULL,
+  `id_annee` int(11) NOT NULL,
+  PRIMARY KEY (`id_classe`),
+  KEY `id_ecole` (`id_ecole`),
+  KEY `id_niveau` (`id_niveau`),
+  KEY `id_annee` (`id_annee`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `detailbulletin`
+--
+
+DROP TABLE IF EXISTS `detailbulletin`;
+CREATE TABLE IF NOT EXISTS `detailbulletin` (
+  `id_detailbull` int(11) NOT NULL AUTO_INCREMENT,
+  `id_bulletin` int(11) NOT NULL,
+  `id_ens` int(11) NOT NULL,
+  `appreciation` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_detailbull`),
+  KEY `id_bulletin` (`id_bulletin`),
+  KEY `id_ens` (`id_ens`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `discipline`
+--
+
+DROP TABLE IF EXISTS `discipline`;
+CREATE TABLE IF NOT EXISTS `discipline` (
+  `id_discipline` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_discipline`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ecole`
+--
+
+DROP TABLE IF EXISTS `ecole`;
+CREATE TABLE IF NOT EXISTS `ecole` (
+  `id_ecole` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_ecole`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `eleve`
+--
+
+DROP TABLE IF EXISTS `eleve`;
+CREATE TABLE IF NOT EXISTS `eleve` (
+  `id_eleve` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `age` int(11) NOT NULL,
+  PRIMARY KEY (`id_eleve`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `enseignement`
+--
+
+DROP TABLE IF EXISTS `enseignement`;
+CREATE TABLE IF NOT EXISTS `enseignement` (
+  `id_ens` int(11) NOT NULL AUTO_INCREMENT,
+  `id_classe` int(11) NOT NULL,
+  `id_discipline` int(11) NOT NULL,
+  `id_professeur` int(11) NOT NULL,
+  PRIMARY KEY (`id_ens`),
+  KEY `id_classe` (`id_classe`),
+  KEY `id_discipline` (`id_discipline`),
+  KEY `id_professeur` (`id_professeur`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `evaluation`
+--
+
+DROP TABLE IF EXISTS `evaluation`;
+CREATE TABLE IF NOT EXISTS `evaluation` (
+  `id_eval` int(11) NOT NULL AUTO_INCREMENT,
+  `id_detailbull` int(11) NOT NULL,
+  `note` int(11) DEFAULT NULL,
+  `appreciation` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_eval`),
+  KEY `id_detailbull` (`id_detailbull`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `inscription`
+--
+
+DROP TABLE IF EXISTS `inscription`;
+CREATE TABLE IF NOT EXISTS `inscription` (
+  `id_inscription` int(11) NOT NULL AUTO_INCREMENT,
+  `id_classe` int(11) NOT NULL,
+  `id_eleve` int(11) NOT NULL,
+  PRIMARY KEY (`id_inscription`),
+  KEY `id_classe` (`id_classe`),
+  KEY `id_eleve` (`id_eleve`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `niveau`
+--
+
+DROP TABLE IF EXISTS `niveau`;
+CREATE TABLE IF NOT EXISTS `niveau` (
+  `id_niveau` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_niveau`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `professeur`
+--
+
+DROP TABLE IF EXISTS `professeur`;
+CREATE TABLE IF NOT EXISTS `professeur` (
+  `id_professeur` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `age` int(11) NOT NULL,
+  PRIMARY KEY (`id_professeur`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `trimestre`
+--
+
+DROP TABLE IF EXISTS `trimestre`;
+CREATE TABLE IF NOT EXISTS `trimestre` (
+  `id_trimestre` int(11) NOT NULL AUTO_INCREMENT,
+  `numero` int(11) DEFAULT NULL,
+  `debut` date DEFAULT NULL,
+  `fin` date DEFAULT NULL,
+  PRIMARY KEY (`id_trimestre`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
