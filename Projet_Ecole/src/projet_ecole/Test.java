36340d639ac3d52c04003fae03a0b16aa2c77aba
Dobid <davidolivares68@gmail.com>
2019-05-31 12:39:03 +0200

    ..

--- Projet_Ecole/src/projet_ecole/Test.java
+++ Projet_Ecole/src/projet_ecole/Test.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package projet_ecole;
-
 import java.sql.SQLException;
 import projet_ecole.Connexion;
 import java.util.ArrayList;
@@ -15,20 +14,21 @@ import java.util.Scanner;
  * @author sebas
  */
 public class Test {
-       public static void main(String[] args) throws SQLException, ClassNotFoundException {
-              Connexion base = new Connexion("ecole", "root", "");
-              ArrayList<String> val = new ArrayList<String>();
-              // val.add("CM2");
-              // val.add("pour");
-              // val.add("67");
-              val.add("2");
-              val.add("Essai");
-              val.add("Affichage");
-              val.add("22");
-
-              Update.modifierEleve(base, val);
-              Reporting.consulterBulletin(base, 1);
-              Query.afficherTable(base, "eleve");
-       }
-
+    public static void main(String[] args) throws SQLException, ClassNotFoundException
+    {
+        Connexion base=new Connexion("ecole", "root", "");
+        ArrayList<String> val=new ArrayList<String>();
+       // val.add("CM2");
+       // val.add("pour");
+       // val.add("67");
+       val.add("2");
+       val.add("Essai");
+       val.add("Affichage");
+       val.add("22");
+       
+        Update.modifierEleve(base, val);
+        Reporting.consulterBulletin(base, 1);
+        Query.afficherTable(base, "eleve");
+    }
+    
 }
