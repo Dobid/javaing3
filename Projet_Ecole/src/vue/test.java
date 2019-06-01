0f16bb5efd8b81803cbf6e5f3ca01a9612bd755f
sebastii1 <sebastien.brunelle75@gmail.com>
2019-06-01 15:36:20 +0200

    compilation

--- Projet_Ecole/src/vue/test.java
+++ Projet_Ecole/src/vue/test.java
@@ -9,9 +9,9 @@ public class test {
 	 public static void main(String[] args) throws ClassNotFoundException, SQLException {
             Database bdd=new Database();
             ArrayList <String> val=new ArrayList<String>();
-            val.add("NS1");
+            val.add("NS2");
             val.add("16");
-            val.add("");
+            val.add("Ne devrait rien afficher");
             bdd.modifierAppEval(val);
 		//new Fenetre2();
 			
